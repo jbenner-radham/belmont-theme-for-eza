08d0b273df2e03eb90c8c4f5b8a2c329e3f2a221
James Benner <hello@jamesbenner.com>
2025-07-09 17:13:42 -0400

    Increase possible size of dummy files for example

--- scripts/make-example-dir.mjs
+++ scripts/make-example-dir.mjs
@@ -1,3 +1,4 @@
+import buffer from 'node:buffer';
 import fs from 'node:fs';
 import path from 'node:path';
 
@@ -20,7 +21,7 @@ const filenames = [
   'file.sh',
   'file.tar.gz',
   'file.toml',
-  'file.yaml',
+  'file.yml',
   'justfile',
   'LICENSE',
   'Makefile',
@@ -40,11 +41,21 @@ const dirnames = ['src'];
 fs.rmSync(examplePath, { force: true, recursive: true });
 fs.mkdirSync(examplePath, { recursive: true });
 filenames.forEach((filename) => {
-  const count =
+  const { MAX_STRING_LENGTH } = buffer.constants;
+  const totalCharCount =
     Math.random() *
     (Math.random() > 0.5 ? 10_000 : 1) *
+    (Math.random() > 0.5 ? 10_000 : 1) *
     (Math.random() > 0.5 ? 10_000 : 1);
-  fs.writeFileSync(path.join(examplePath, filename), '0'.repeat(count));
+  const createCharCount = Math.min(MAX_STRING_LENGTH, totalCharCount);
+  const appendCharCountCandidate = Math.max(
+    totalCharCount - MAX_STRING_LENGTH,
+    0
+  );
+  const appendCharCount = Math.min(MAX_STRING_LENGTH, appendCharCountCandidate);
+  const filePath = path.join(examplePath, filename);
+  fs.writeFileSync(filePath, '0'.repeat(createCharCount));
+  fs.appendFileSync(filePath, '0'.repeat(appendCharCount));
 });
 dirnames.forEach((dirname) =>
   fs.mkdirSync(path.join(examplePath, dirname), { recursive: true })
